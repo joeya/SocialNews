5a09ce4a5eed7716ed2dc05117cb7544ff95ab6e
杨亚 <yangya@live.com>
2012-11-29 16:21:37 +0800

    update

---
+++ SocialNews.Data/AuthorRepository.cs
@@ -0,0 +1,18 @@
+﻿using SocialNews.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+namespace SocialNews.Data
+{
+    public class AuthorRepository : BaseRepository<Author>
+    {
+        static string sharedSecret = "social!@news";
+        public bool ValidateUser(string userName, string password)
+        {
+            var authors = this.GetAllFilteredBy(x => x.Login.Equals(userName) && x.Password.Equals(password));
+            return (authors.Count() == 1);
+        }
+    }
+}
